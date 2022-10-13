837194a3b024e64c10fa9a8ec0abd1993af4e400
ahmed karim <ahmed.karim.lahmar@gmail.com>
2022-10-13 16:09:37 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Project Manager");
+});
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
