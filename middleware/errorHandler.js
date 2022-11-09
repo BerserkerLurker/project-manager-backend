3514930a1bc63e24ce4322b208c1dc62507409a8
ahmed karim <ahmed.karim.lahmar@gmail.com>
2022-11-09 10:26:41 +0100

    feat: handle mongo and mongoose errors.

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -6,6 +6,25 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Something went wrong, try again later.",
   };
 
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value.`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id : ${err.value}.`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+  
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
