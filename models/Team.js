b19514be13f36a927a9fb9c75c868480c0b71073
ahmed karim <ahmed.karim.lahmar@gmail.com>
2023-01-28 09:40:39 +0100

    feat: add team members to Team schema

--- models/Team.js
+++ models/Team.js
@@ -6,6 +6,19 @@ const TeamSchema = new mongoose.Schema({
     required: [true, "Please provide a name."],
     unique: true,
   },
+  members: [
+    new mongoose.Schema({
+      memberId: { type: mongoose.Types.ObjectId, ref: "User" },
+      status: {
+        type: String,
+        enum: {
+          values: ["notMember", "rejected", "pending", "accepted"],
+          message:
+            "Status should be either notMember, rejected, pending or accepted.",
+        },
+      },
+    }),
+  ],
 });
 
 module.exports = mongoose.model("Team", TeamSchema);
