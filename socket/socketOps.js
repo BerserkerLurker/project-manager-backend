a95b432468fb7ec1bde3f1bcb4ed78642131790d
ahmed karim <ahmed.karim.lahmar@gmail.com>
2023-02-20 15:45:42 +0100

    feat: add subscribe and unsubscribe to chat room

--- socket/socketOps.js
+++ socket/socketOps.js
@@ -1,7 +1,7 @@
 function socketOps() {
   let users = [];
   global.io.on("connection", (socket) => {
-    console.log(socket.handshake.auth);
+    // console.log(socket.handshake.auth);
     console.log("A User connected: " + socket.id);
 
     var userInfo = new Object();
@@ -12,6 +12,7 @@ function socketOps() {
     // console.log(users);
     global.io.emit("users", users);
 
+    // logout event
     socket.on("end", () => {
       socket.disconnect();
     });
@@ -24,10 +25,35 @@ function socketOps() {
       global.io.emit("users", users);
     });
 
-    socket.on("chat", (msg) => {
+    socket.on("chat", (msg, callback) => {
       console.log("message: " + msg);
+      callback({ msg: `got it! ${new Date()}` });
     });
 
+    // subscribe to a chat room
+    socket.on("subscribe", (room, otherUserId) => {
+      const userSockets = users.filter((user) => user.userId === otherUserId);
+      userSockets.map((userInfo) => {
+        console.log(global.io.of("/").sockets.keys());
+        console.log(global.io.of("/").sockets.get(userInfo.clientId).connected);
+        const socketConn = global.io.of("/").sockets.get(userInfo.clientId);
+        // const socketConn = global.io.sockets.connected(userInfo.clientId);
+        if (socketConn.connected) {
+          socketConn.join(room);
+          console.log(global.io.of("/").adapter.rooms);
+        }
+      });
+      socket.join(room);
+      global.io.to(room).emit(`Welcome to ${room} ${userInfo.userName}`);
+      // console.log(global.io.of("/").adapter.rooms);
+    });
+
+    // mute a chat room
+    socket.on("unsubscribe", (room) => {
+      socket.leave(room);
+      global.io.to(room).emit(`${userInfo.userName} left the room`);
+      console.log(global.io.of("/").adapter.rooms);
+    });
   });
 }
 
